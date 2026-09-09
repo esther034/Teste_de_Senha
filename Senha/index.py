e81e7a8af36935ff3c6c5c80742490b765c664d7
Esther Massuchetto <estherclaro.araujo@gmail.com>
2026-09-09 16:37:27 -0300

    código do projeto

--- Senha/index.py
+++ Senha/index.py
@@ -7,16 +7,36 @@ if len(senha)  >= 8:
 
 # Verifica letra maiúscula
 for caractere in senha:
-    if caractere.islower():
+    if caractere.isupper():
         pontuacao += 1
         break
 
 # Verifica letra minúscula
 for caractere in senha:
-    if caractere.isdigit():
+    if caractere.islower():
         pontuacao += 1
         break
 
-# Verifica 
+# Verifica número
+for caractere in senha:
+    if caractere.isdigit():
+        pontuacao += 1 
+        break
 
+# Verifica caractere especial
+especiais = "!@#$%&*()-+=_"
 
+for caractere in senha:
+    if caractere in especiais:
+        pontuacao += 1
+        break
+
+    # Classificação 
+    if pontuacao <= 2:
+        print('Força da senha: FRACA')
+    elif pontuacao <= 4:
+        print('Força da senha: MÉDIA')
+    else:
+        print('Força da senha: FORTE')
+
+    print(f'Pontuação: {pontuacao}/5')
