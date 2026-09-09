bfe23a88fce0ac9cc794a884ab5bcfad8fcbfa9c
Esther Massuchetto <estherclaro.araujo@gmail.com>
2026-09-09 20:43:23 -0300

    Adicionei análise detalhada da senha

--- Senha/index.py
+++ Senha/index.py
@@ -1,42 +1,122 @@
-senha = input('Digite sua senha: ')
-pontuacao = 0
+print('='*30)
+print('     VERIFICADOR DE SENHA')
+print('='*30)
 
-#Verifica o Tamanho
-if len(senha)  >= 8:
-    pontuacao += 1
+senhas_comuns = [
+    '123456',
+    '12345678',
+    'password',
+    'qwerty',
+    'admin',
+]
 
-# Verifica letra maiúscula
-for caractere in senha:
-    if caractere.isupper():
+while True:
+
+    senha = input('\nDigite sua senha: ')
+
+    pontuacao = 0
+
+    # Tamanho
+    if len(senha) >= 8:
         pontuacao += 1
-        break
+        print('✔️ Possui 8 ou mais caracteres')
+    else:
+        print('x Possui menos de 8 caracteres')
 
-# Verifica letra minúscula
-for caractere in senha:
-    if caractere.islower():
+    # Maiúscula
+    tem_maiuscula = False
+
+    for caractere in senha:
+        if caractere.isupper():
+            tem_maiuscula = True
+            break
+
+    if tem_maiuscula:
         pontuacao += 1
-        break
+        print('✔️ Possui letra maiúscula')
+    else:
+        print('x Não possui letra maiúscula')
 
-# Verifica número
-for caractere in senha:
-    if caractere.isdigit():
-        pontuacao += 1 
-        break
+    # Minúscula
+    tem_maiuscula = False
+    
+    for caractere in senha:
+         if caractere.islower():
+            tem_maiuscula = True
+            break
+    
+    if tem_maiuscula:
+         pontuacao += 1
+         print('✔️ Possui letra minúscula')
+    else:
+        print('x Não possui letra minuscula')
 
-# Verifica caractere especial
-especiais = "!@#$%&*()-+=_"
+    # Número
+    tem_numero = False
+    
+    for caractere in senha:
+        if caractere.isdigit():
+            tem_numero = True
+            break
+    
+    if tem_numero:
+         pontuacao += 1
+         print('✔️ Possui número')
+    else:
+        print('x Não possui número')
 
-for caractere in senha:
-    if caractere in especiais:
+    # Caractere especial
+    especiais = '!@#$%&*()-_=+'
+    tem_especial = False
+
+    for caractere in senha:
+        if caractere in especiais:
+            tem_especial = True
+            break
+
+    if tem_especial:
         pontuacao += 1
+        print('✔️ Possui caractere especial')
+    else:
+        print('x Não possui caractere especial')
+
+    # Verificação de senha comum
+    if senha.lower() in senhas_comuns:
+        print('⚠️ ATENÇÃO: essa senha é muito comum')
+
+    # Resultado 
+    print('\n='*30)
+    print('           RESULTADO')
+    print('='*30)
+
+    print(f'Pontuação: {pontuacao}/5')
+
+    if pontuacao <= 2:
+        print('Força: FRACA')
+        print('Dica: tente adicionar letras,números e símbolos.')
+
+    elif pontuacao <= 4:
+        print('Força: MÉDIA')
+        print('Dica: sua senha pode ser melhorada.')
+
+    else:
+        print('Força: FORTE')
+        print('Sua senha atende aos critérios básicos.')
+
+    continuar = input('\nDeseja tentar outra senha? (s/n): ')
+
+    if continuar.lower() != 's':
+        print('\nPrograma encerrado.')
         break
 
-# Classificação 
-if pontuacao <= 2:
-    print('Força da senha: FRACA')
-elif pontuacao <= 4:
-    print('Força da senha: MÉDIA')
-else:
-    print('Força da senha: FORTE')
+        
 
-print(f'Pontuação: {pontuacao}/5')
+    
+
+
+
+
+
+
+
+
